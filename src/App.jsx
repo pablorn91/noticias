84348a750d594dbf522596af4cacef136c4b2fc2
Pablo Rodriguez Nunes <pmrn24@gmail.com>
2022-02-23 12:29:47 -0400

    Inicio Proyecto

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import Header from "./components/Header"
+
+function App() {
+
+  return (
+    <>
+      <Header
+        titulo='Buscador de Noticias'
+      />
+
+      <div className="container white">
+        <h1>Formulario aquí</h1>
+      </div>
+    </>
+  )
+}
+
+export default App
